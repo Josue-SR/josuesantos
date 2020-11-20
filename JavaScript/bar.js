eea065af723931d02ae06327fef7603ef5ef38d3
Josue Santos <santozjosue831@gmail.com>
2020-11-19 21:07:52 -0800

    Fixed Menu Animation
    Made a menu for mobile devices so the words weren't all cluster together when the device was small. I made small fixes to make the website responsive and played around with design.

--- JavaScript/bar.js
+++ JavaScript/bar.js
@@ -1,8 +1,8 @@
 window.onscroll = function() {
-	myFunction()
+	myFunction();
 };
 
-var navbar = document.querySelector("header");
+var navbar = document.querySelector(".navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
@@ -11,4 +11,18 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+//Menu Config
+const mainMenu = document.querySelector('.menu-content');
+const closeMenu = document.querySelector('.fa-times');
+const openMenu = document.querySelector('.fa-bars');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'block';
+}
+function close(){
+    mainMenu.style.display = 'none';
 }
