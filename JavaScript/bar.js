74257a396fb944cd559fedf8b0cf79ded73ea1c4
Josue Santos <santozjosue831@gmail.com>
2021-05-18 20:49:04 -0700

    Fixed nav bar bug
    Made a few changes it to make it responsive and fixed the nav bar bug where the text would get glitched

--- JavaScript/bar.js
+++ JavaScript/bar.js
@@ -1,3 +1,11 @@
+//Menu Config
+const page = document.querySelector('html');
+const mainMenu = document.querySelector('.menu-content');
+const closeMenu = document.querySelector('.fa-times');
+const openMenu = document.querySelector('.fa-bars');
+const hide = document.querySelector('.times');
+const see = document.querySelector('.bars');
+
 window.onscroll = function() {
 	myFunction();
 };
@@ -12,17 +20,25 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
-//Menu Config
-const mainMenu = document.querySelector('.menu-content');
-const closeMenu = document.querySelector('.fa-times');
-const openMenu = document.querySelector('.fa-bars');
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
 function show(){
     mainMenu.style.display = 'block';
+    see.style.visibility = 'hidden';
+    hide.style.visibility = 'visible';
+    page.style.overflow = 'hidden';
+    mainMenu.style.height = '100%'
 }
 function close(){
     mainMenu.style.display = 'none';
+    see.style.visibility = 'visible';
+    hide.style.visibility = 'hidden';
+    page.style.overflow = 'visible';
+    mainMenu.style.height = '9%';
 }
+
+
+
+
